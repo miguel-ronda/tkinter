b8d19f6c10e3141896bb5732e699a6bb6fab884d
miguel-ronda <miguel_ronda@estudante.sesisenai.org.br>
2025-02-25 19:18:50 -0300

    .

--- TKINTERCOLUNALINHA.py
+++ TKINTERCOLUNALINHA.py
@@ -3,23 +3,21 @@ i=Tk()
 i.title("Programa Financeiro")
 i.geometry('980x720+250+30')
 
-
-lb1=Label(i,text="login",bg="yellow")
 #componente .grid serve tambem para posicionar utiliANDO INDICATIVO DE LINHA (ROW) E COLUNA (COLUMN).
-lb1.grid(row=2,column=2)
-
+lb1=Label(i,text="login",bg="yellow")
+lb1.place(x=1130,y=100)
 
 lb2=Label(i,text="senha",bg="red")
-lb2.grid(row=1,column=2)
+lb2.place(x=1130,y=150)
 
 ed1=Entry(i)
-ed1.grid(row=1,column=2)
+ed1.place(x=1,y=2)
 
 ed2=Entry(i)
-ed2.grid(row=2,column=2)
+ed2.place(x=2,y=2)
 
 bt1 = Button(i,text="login")
-bt1.grid(row=2,column=4)
+bt1.place(x=465,y=500)
 
 
 i.mainloop()
